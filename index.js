a4a0b9ca1dd61d4230b77b419dca227db9a1817a
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2023-10-09 12:59:36 -0600

    fix pm/am bug and add addtional functionality for displaying 0 if h,m,s <10.

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ function updateClock(){
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-    let ampm = "AM"
+    var ampm = h >= 12 ? 'PM' : 'AM';
+    
+    
 
     if (h>12){
         h=h-12
-        ampm= "PM"
+       
     }
+
+    h = h< 10 ? "0" +h : h;
+    m = m< 10 ? "0" +m : m;
+    s = s< 10 ? "0" +s : s;
+
+
+
 
     hourE1.innerText = h;
     minuteE1.innerText = m;
     secondE1.innerText =s;
-    ampmE1.innerText= ampm;
+    ampmE1.innerHTML= ampm;
     console.log(ampm)
     setTimeout(()=>{
         updateClock()
